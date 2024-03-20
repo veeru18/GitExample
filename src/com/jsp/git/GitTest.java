ccd74780ced865c9b0ae23d5b8b93b70cef42550
veeru18 <veereshta2gmail.com>
2024-03-20 13:44:59 +0530

    main login functionality

--- src/com/jsp/git/GitTest.java
+++ src/com/jsp/git/GitTest.java
@@ -5,4 +5,7 @@ public class GitTest {
 	// f2 at dev
 	// f3 at dev branch
 	// f4 at login branch -- extended from login branch
+	public static void main(String[] args) {
+		System.out.println("Welcome to Our GitTest Login Page");
+	}
 }
